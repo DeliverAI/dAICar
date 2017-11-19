ec361ab08abe3304e5bbbd0ccdeedbf0dadbfaad
soheil-hamidi <soheilhamidi@me.com>
2017-11-19 05:04:32 -0500

    Done! Kinda...

--- car.py
+++ car.py
@@ -5,67 +5,88 @@ import json
 import requests
 
 
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setmode(GPIO.BOARD)
 
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
-
-Motor2A = 19
-Motor2B = 21
-Motor2E = 23
-
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
-
-GPIO.setup(Motor2A,GPIO.OUT)
-GPIO.setup(Motor2B,GPIO.OUT)
-GPIO.setup(Motor2E,GPIO.OUT)
 
 def driveForward(time):
+
     GPIO.setmode(GPIO.BOARD)
+    Motor1A = 16
+    Motor1B = 18
+    Motor1E = 22
 
+    Motor2A = 19
+    Motor2B = 21
+    Motor2E = 23
+
+    GPIO.setup(Motor1A, GPIO.OUT)
+    GPIO.setup(Motor1B, GPIO.OUT)
+    GPIO.setup(Motor1E, GPIO.OUT)
+
+    GPIO.setup(Motor2A, GPIO.OUT)
+    GPIO.setup(Motor2B, GPIO.OUT)
+    GPIO.setup(Motor2E, GPIO.OUT)
     print "Going forwards"
-    GPIO.output(Motor1A,GPIO.HIGH)
-    GPIO.output(Motor1B,GPIO.LOW)
-    GPIO.output(Motor1E,GPIO.HIGH)
+    GPIO.output(Motor1A, GPIO.HIGH)
+    GPIO.output(Motor1B, GPIO.LOW)
+    GPIO.output(Motor1E, GPIO.HIGH)
 
-    GPIO.output(Motor2A,GPIO.HIGH)
-    GPIO.output(Motor2B,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.HIGH)
+    GPIO.output(Motor2A, GPIO.HIGH)
+    GPIO.output(Motor2B, GPIO.LOW)
+    GPIO.output(Motor2E, GPIO.HIGH)
 
     sleep(time)
-    GPIO.output(Motor1E,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.LOW)
+    GPIO.output(Motor1E, GPIO.LOW)
+    GPIO.output(Motor2E, GPIO.LOW)
     GPIO.cleanup()
+
 
 def turnLeft(time):
     GPIO.setmode(GPIO.BOARD)
+    Motor1A = 16
+    Motor1B = 18
+    Motor1E = 22
+
+    Motor2A = 19
+    Motor2B = 21
+    Motor2E = 23
+
+    GPIO.setup(Motor1A, GPIO.OUT)
+    GPIO.setup(Motor1B, GPIO.OUT)
+    GPIO.setup(Motor1E, GPIO.OUT)
+
+    GPIO.setup(Motor2A, GPIO.OUT)
+    GPIO.setup(Motor2B, GPIO.OUT)
+    GPIO.setup(Motor2E, GPIO.OUT)
 
     print "Turnning"
-    GPIO.output(Motor1A,GPIO.LOW)
-    GPIO.output(Motor1B,GPIO.HIGH)
-    GPIO.output(Motor1E,GPIO.HIGH)
+    GPIO.output(Motor1A, GPIO.LOW)
+    GPIO.output(Motor1B, GPIO.HIGH)
+    GPIO.output(Motor1E, GPIO.HIGH)
 
-    GPIO.output(Motor2A,GPIO.HIGH)
-    GPIO.output(Motor2B,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.HIGH)
+    GPIO.output(Motor2A, GPIO.HIGH)
+    GPIO.output(Motor2B, GPIO.LOW)
+    GPIO.output(Motor2E, GPIO.HIGH)
 
     sleep(time)
-    GPIO.output(Motor1E,GPIO.LOW)
-    GPIO.output(Motor2E,GPIO.LOW)
+    GPIO.output(Motor1E, GPIO.LOW)
+    GPIO.output(Motor2E, GPIO.LOW)
     GPIO.cleanup()
 
-while(True):
-    r = requests.get('https://hackwestern-8e5fa.firebaseio.com/who_ordered/order_made.json')
-    if(r.json()):
-        address = requests.get('https://hackwestern-8e5fa.firebaseio.com/who_ordered/address.json')
 
-        car_instructions = requests.get('https://hackwestern-8e5fa.firebaseio.com/car_instructions.json')
+while(True):
+    r = requests.get(
+        'https://hackwestern-8e5fa.firebaseio.com/who_ordered/order_made.json')
+    if(r.json()):
+        address = requests.get(
+            'https://hackwestern-8e5fa.firebaseio.com/who_ordered/address.json')
+
+        car_instructions = requests.get(
+            'https://hackwestern-8e5fa.firebaseio.com/car_instructions.json')
         json_data = json.loads(car_instructions.text)
         try:
-            instructions = json_data[address.text.encode('utf8').replace("\"","")]
+            instructions = json_data[address.text.encode(
+                'utf8').replace("\"", "")]
         except KeyError, e:
             instructions = json_data['1 University Road']
 
@@ -75,7 +96,8 @@ while(True):
         turnLeft(left)
         driveForward(forward)
 
-        requests.patch('https://hackwestern-8e5fa.firebaseio.com/.json', data = json.dumps({"package_arrived": True}))
+        requests.patch('https://hackwestern-8e5fa.firebaseio.com/.json',
+                       data=json.dumps({"package_arrived": True}))
 
         openBox()
 
