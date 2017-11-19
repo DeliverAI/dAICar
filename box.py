fb99d92c3bd81cbcae8bc014b3b6e8e1c84cd4e5
soheil-hamidi <soheilhamidi@me.com>
2017-11-19 03:35:20 -0500

    cleanning up!

---
+++ box.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import requests
+import json
+
+def openBox():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18, GPIO.OUT)
+    pwm = GPIO.PWM(18, 50)
+    pwm.start(1)
+    flag = True
+
+    while(flag):
+        r = requests.get('https://hackwestern-8e5fa.firebaseio.com/package_unlock.json')
+        if(r.json()):
+            pwm.ChangeDutyCycle(8)
+            sleep(10)
+            requests.patch('https://hackwestern-8e5fa.firebaseio.com/.json', data = json.dumps({"package_unlock": False}))
+            # print(r.json())
+        else:
+            # print("Hello!")
+            pwm.ChangeDutyCycle(1)
+            pwm.stop()
+            flag = False
+        sleep(1)
