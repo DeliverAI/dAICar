2980456a978c2fc14ba3c79a2326f1958b469300
soheil-hamidi <soheilhamidi@me.com>
2017-11-19 04:48:16 -0500

    Done! Kinda...

--- car.py
+++ car.py
@@ -1,9 +1,13 @@
-import RPi.GPIO as GPIO
 from time import sleep
-from box import openBox
+# from box import openBox
+import json
+import requests
+# import RPi.GPIO as GPIO
 
-openBox()
 
+# openBox()
+
+#
 # GPIO.setmode(GPIO.BOARD)
 #
 # Motor1A = 16
@@ -21,18 +25,68 @@ openBox()
 # GPIO.setup(Motor2A,GPIO.OUT)
 # GPIO.setup(Motor2B,GPIO.OUT)
 # GPIO.setup(Motor2E,GPIO.OUT)
-#
-# print "Going forwards"
-# GPIO.output(Motor1A,GPIO.HIGH)
-# GPIO.output(Motor1B,GPIO.LOW)
-# GPIO.output(Motor1E,GPIO.HIGH)
-#
-# GPIO.output(Motor2A,GPIO.HIGH)
-# GPIO.output(Motor2B,GPIO.LOW)
-# GPIO.output(Motor2E,GPIO.HIGH)
-#
-# sleep(2)
-#
+
+
+
+
+print(instructions)
+print(left)
+print(forward)
+
+
+while(True):
+    r = requests.get('https://hackwestern-8e5fa.firebaseio.com/who_ordered/order_made.json')
+    if(r.json()):
+        address = requests.get('https://hackwestern-8e5fa.firebaseio.com/who_ordered/address.json')
+
+        car_instructions = requests.get('https://hackwestern-8e5fa.firebaseio.com/car_instructions.json')
+        json_data = json.loads(car_instructions.text)
+        try:
+            instructions = json_data[address.text.encode('utf8').replace("\"","")]
+        except KeyError, e:
+            instructions = json_data['1 University Road']
+
+        left = instructions['left']
+        forward = instructions['forward']
+
+        turnLeft(left)
+        driveForward(forward)
+
+        requests.patch('https://hackwestern-8e5fa.firebaseio.com/.json', data = json.dumps({"package_arrived": True}))
+
+        openBox()
+
+
+def driveForward(time):
+    print "Going forwards"
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1E,GPIO.HIGH)
+
+    GPIO.output(Motor2A,GPIO.HIGH)
+    GPIO.output(Motor2B,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.HIGH)
+
+    sleep(time)
+    GPIO.output(Motor1E,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.LOW)
+    GPIO.cleanup()
+
+def turnLeft(time):
+    print "Turnning"
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.HIGH)
+    GPIO.output(Motor1E,GPIO.HIGH)
+
+    GPIO.output(Motor2A,GPIO.HIGH)
+    GPIO.output(Motor2B,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.HIGH)
+
+    sleep(time)
+    GPIO.output(Motor1E,GPIO.LOW)
+    GPIO.output(Motor2E,GPIO.LOW)
+    GPIO.cleanup()
+
 # print "Going backwards"
 # GPIO.output(Motor1A,GPIO.LOW)
 # GPIO.output(Motor1B,GPIO.HIGH)
@@ -44,16 +98,6 @@ openBox()
 #
 # sleep(2)
 #
-# print "Turnning"
-# GPIO.output(Motor1A,GPIO.LOW)
-# GPIO.output(Motor1B,GPIO.HIGH)
-# GPIO.output(Motor1E,GPIO.HIGH)
-#
-# GPIO.output(Motor2A,GPIO.HIGH)
-# GPIO.output(Motor2B,GPIO.LOW)
-# GPIO.output(Motor2E,GPIO.HIGH)
-#
-# sleep(1)
 #
 # print "Now stop"
 # GPIO.output(Motor1E,GPIO.LOW)
